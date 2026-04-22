6c51c801c2dd86fde2cfbdd7e5b88dbba29ae736
IvanceteG <garciacuestaivan@fpllefia.com>
2026-04-22 19:10:24 +0200

    Aplicamos CORS

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import mascotasRouter from './routes/mascotasRouter.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
